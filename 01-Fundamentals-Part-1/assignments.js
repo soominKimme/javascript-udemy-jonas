4f19804cd4b7d384266c930618667fe8ac73fc8d
Soomin Kim <96629550+soominKimme@users.noreply.github.com>
2022-12-01 23:06:15 +0000

    First commit

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,9 @@
+// JavaScript Fundamentals – Part 1
+// 1. LECTURE: Values and Variables.
+let country = "Republic of Korea";
+let continent = "East Asia";
+let population = 60000000;
+
+console.log("Country : " + country);
+console.log("Continent : " + continent);
+console.log("Population : " + population);
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,15 @@
+let js = "amazing";
+console.log(59 + 40 - 19);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Soomin" // let variable = "value" 
+console.log(firstName);
+
+let myFirstJob = "Teacher";
+let myCurrentJob = "Web Developeer";
+let PI = 3.1415;
+
+console.log(myFirstJob);
+
